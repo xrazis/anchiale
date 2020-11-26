8c5df2bd6250d2c1520a527b7997fe4fe4341ff8
Haris Razis <dev@xrazis.com>
2020-11-26 10:56:45 +0200

    🔨 Split Points.vue => Canva & Table

---
+++ web/src/components/Canva.vue
@@ -0,0 +1,71 @@
+<template>
+  <canvas id="chart"></canvas>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import { Component, Prop } from 'vue-property-decorator';
+import Chart from 'chart.js';
+
+export default class Canva extends Vue {
+  @Prop() readonly points!: [];
+
+  mounted() {
+    const ctx = document.getElementById('chart') as HTMLCanvasElement;
+    const myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [
+          '-60m',
+          '-55m',
+          '-50m',
+          '-45m',
+          '-40m',
+          '-35m',
+          '-30m',
+          '-25m',
+          '-20m',
+          '-15m',
+          '-10m',
+          '-5m',
+        ],
+        datasets: [
+          {
+            // one line graph
+            label: 'Number of Moons',
+            data: [0, 0, 1, 2, 37, 42, 27, 14, null, 10, 10, 55],
+
+            borderColor: [
+              '#36495d',
+              '#36495d',
+              '#36495d',
+              '#36495d',
+              '#36495d',
+              '#36495d',
+              '#36495d',
+              '#36495d',
+            ],
+            borderWidth: 3,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                padding: 5,
+                max: 60,
+              },
+            },
+          ],
+        },
+      },
+    });
+  }
+}
+</script>
+
+<style></style>
--- web/src/components/Points.vue
+++ web/src/components/Points.vue
@@ -34,34 +34,15 @@
       </div>
     </div>
     <div v-if="isChart">
-      <canvas id="chart"></canvas>
+      <Canva :points="points" />
     </div>
-    <div v-if="isTable">
-      <table
-        class="table 
-        table is-striped
-        table is-hoverable
-        has-text-left
-        table is-fullwidth"
-      >
-        <thead>
-          <tr>
-            <th><abbr title="Device">Device id</abbr></th>
-            <th><abbr title="Time">Time</abbr></th>
-            <th><abbr title="Temp">Measurement</abbr></th>
-          </tr>
-        </thead>
-        <tr v-for="(point, index) in points" :key="index">
-          <td class="is-family-monospace">{{ point.client }}</td>
-          <td>{{ new Date(point.time).toLocaleString() }}</td>
-          <td>{{ point.value }}°C</td>
-        </tr>
-      </table>
+    <div v-else-if="isTable">
+      <Table :points="points" />
     </div>
     <div v-else-if="hasData">
-      <span class="panel-block"
-        >Fetched data, try creating a chart or table!</span
-      >
+      <span class="panel-block">
+        Fetched data, try creating a chart or table!
+      </span>
     </div>
     <div v-else-if="error">
       <span class="panel-block">{{ error }}</span>
@@ -73,11 +54,17 @@
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component';
+import { Options, Vue } from 'vue-class-component';
 import axios from 'axios';
-import Chart from 'chart.js';
-import ChartFormat from '../class/ChartFormat';
+import Canva from '@/components/Canva.vue';
+import Table from '@/components/Table.vue';
 
+@Options({
+  components: {
+    Canva,
+    Table,
+  },
+})
 export default class Points extends Vue {
   private error = '';
   private isChart = false;
@@ -116,62 +103,8 @@ export default class Points extends Vue {
   }
 
   createChart() {
-    const formatedData = new ChartFormat(this.points);
     this.isTable = false;
     this.isChart = true;
-    const ctx = document.getElementById('chart') as HTMLCanvasElement;
-    const myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: [
-          '-60m',
-          '-55m',
-          '-50m',
-          '-45m',
-          '-40m',
-          '-35m',
-          '-30m',
-          '-25m',
-          '-20m',
-          '-15m',
-          '-10m',
-          '-5m',
-        ],
-        datasets: [
-          {
-            // one line graph
-            label: 'Number of Moons',
-            data: [0, 0, 1, 2, 37, 42, 27, 14, null, 10, 10, 55],
-
-            borderColor: [
-              '#36495d',
-              '#36495d',
-              '#36495d',
-              '#36495d',
-              '#36495d',
-              '#36495d',
-              '#36495d',
-              '#36495d',
-            ],
-            borderWidth: 3,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                padding: 5,
-                max: 60,
-              },
-            },
-          ],
-        },
-      },
-    });
   }
 
   createTable() {
